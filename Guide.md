0806554a4c5f370795596becbfea89a003532dc5
jayakvlr <jayakvlr@gmail.com>
2024-02-14 21:42:40 +0000

    base code

---
+++ Guide.md
@@ -0,0 +1,8 @@
+Step 1
+1 Install Requirements.txt
+pip -r requirements.txt
+2 create an app in djago project
+python manage.py startapp openPages
+3 create a scrapy project and manually add that inside the app that we created and create a tasks.py file extra
+
+
---
+++ openPages/pageScraper/settings.py
@@ -0,0 +1,113 @@
+# Scrapy settings for pageScraper project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from __future__ import unicode_literals
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyAIGuider.settings")
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "../../.."))
+
+MEDIA_ALLOW_REDIRECTS = True
+
+BOT_NAME = "openPages"
+LOG_LEVEL='DEBUG'
+
+SPIDER_MODULES = ['scrapy_django_dashboard.spiders','openPages.spider']
+#NEWSPIDER_MODULE = "pageScraper.spiders"
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#    "Accept-Language": "en",
+#}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "pageScraper.middlewares.PagescraperSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    "pageScraper.middlewares.PagescraperDownloaderMiddleware": 543,
+#}
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    'scrapy_django_dashboard.pipelines.DjangoImagesPipeline': 200,
+    'scrapy_django_dashboard.pipelines.ValidationPipeline': 400,
+    'openPages.pageScraper.pipelines.PagescraperPipeline': 800,
+}
+IMAGES_THUMBS = {
+    'medium': (50, 50),
+    'small': (25, 25),
+}
+
+DSCRAPER_IMAGES_STORE_FORMAT = 'ALL'
+DSCRAPER_LOG_ENABLED = True
+DSCRAPER_LOG_LEVEL = 'ERROR'
+DSCRAPER_LOG_LIMIT = 5
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
